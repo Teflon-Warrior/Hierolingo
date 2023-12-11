f74d4aacc5a959e253943281e91ef5c909f3c8cc
Smith, Aidan Henry <aidhsmit@iu.edu>
2023-12-10 23:06:19 -0500

    Re-adding the files so route is correct

---
+++ project.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
+
+<body>
+
+    <!-- NAVIGATION -->
+    <nav id="mySidenav" class="sidenav">
+        <ul>
+            <li><a class="closebtn">&times;</a></li>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="project.html">Project</a></li>
+            <li><a href="team.html">Team</a></li>
+            <li><a href="video.html">Video</a></li>
+        </ul>
+    </nav>
+
+
+    <!-- MAIN CONTENT -->
+
+    <header>
+
+        <div class="openbtn">
+            <span class="material-symbols-outlined menu-button">menu</span>
+            <span class="menu-text">menu</span>
+        </div>
+
+        <div class="all-over-bkg"></div>
+
+        <h1>Project</h1>
+
+    </header>
+
+    <div class="racing-stripes"></div>
+
+    <div class="project-about">
+        <h2>About DataGlpyh</h2>
+
+        <p>Our team has a pursuant interest in history but especially in the subject surrounding ancient Egypt and its
+            culture. While there is a lot we do know about the culture, there are still many scripts that are left
+            undeciphered. We would love to be able to understand these texts to learn more about their society, but they
+            are unfortunately written using hieroglyphs. After conducting research into online language learning
+            services, there are no low cost options for learning to decipher hieroglyphs. So, in order to gain a better
+            understanding of Ancient Egyptian society we intend on creating Dataglyph. Dataglyph will be a free to use
+            website that will provide lessons to users in order to teach them the language. The overarching goal of our
+            project is to increase the number of individuals that have the ability to read hieroglyphs. By increasing
+            this number of individuals, not only will the language not become extinct, but the number of deciphered
+            scripts will continue to increase before the old scripts are lost to time.
+        </p>
+    </div>
+    <div class="sustain">
+        <h2>Sustainabiltiy</h2>
+        <p>
+            Providing the public with the ability to learn to read ancient Egyptian hieroglyphs has many benefits.
+            Increasing the number of individuals that can decipher ancient Egyptian scripts can help the world
+            understand more about their society.
+        </p>
+
+    </div>
+
+    <script src="js/nav.js"></script>
+
+</body>
+
+</html>
---
+++ video.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Video</title>
+
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/styles.css">
+
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+</head>
+
+<body>
+
+	<!-- NAVIGATION -->
+	<nav id="mySidenav" class="sidenav">
+		<ul>
+			<li><a class="closebtn">&times;</a></li>
+			<li><a href="index.html">Home</a></li>
+			<li><a href="project.html">Project</a></li>
+			<li><a href="team.html">Team</a></li>
+			<li><a href="video.html">Video</a></li>
+		</ul>
+	</nav>
+
+
+	<!-- MAIN CONTENT -->
+
+	<header>
+
+		<div class="openbtn">
+			<span class="material-symbols-outlined menu-button">menu</span>
+			<span class="menu-text">menu</span>
+		</div>
+
+		<div class="all-over-bkg"></div>
+
+		<h1>Video</h1>
+
+	</header>
+
+	<div class="racing-stripes"></div>
+
+	<div class="project">
+		<h2>Our Pitch to You</h2>
+		<p>Below, you can watch a video where we talk more in depth about our project.</p>
+	</div>
+
+	<div class="video">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/l87hcx_tCng?si=MoenYG9kPnvMX5fJ"
+			title="YouTube video player" frameborder="0"
+			allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen></iframe>
+	</div>
+
+	<script src="js/nav.js"></script>
+
+</body>
+
+</html>
