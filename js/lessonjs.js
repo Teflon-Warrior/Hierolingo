d568957f0f06d2ba4b12c0e662de5260305cc717
maxarno <maxarno@iu.edu>
2024-01-17 10:16:43 -0500

    Creating lesson.php and flash card to go with it.

---
+++ js/lessonjs.js
@@ -0,0 +1,9 @@
+function termClick() {
+  term.style.display="none";
+  definition.style.display="block";
+}
+
+function definitionClick() {
+  definition.style.display="none";
+  term.style.display="block";
+}
---
+++ lesson.php
@@ -0,0 +1,22 @@
+<html>
+
+<head>
+  <link href="./css/lessoncss.css" rel="stylesheet" type="text/css">
+  <script type="text/javascript" src="./js/lessonjs.js"></script>
+</head>
+
+<body>
+  <h1 align="center"> Lesson # <h1>
+
+  <div class="flash" onclick="termClick();" id="term">
+                
+        
+        <img src="./img/shr.svg">
+        
+  </div>
+
+  <div class="flash" hidden onclick="definitionClick();" id="definition">
+        <p class="flashText"> Definition Goes Here</p>
+  </div>
+
+</body>
