156008a6c35f9e5b0cdcec1a1b81396bdc69a404
Luke Harris <johaharr@iu.edu>
2024-01-31 10:30:25 -0500

    created the resource page and added some links

---
+++ resource.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> some additional resources to access to learn the egpyitan Language</h1>
+  <div>
+    <!-- Resource 1-->    
+  <a href="https://www.egyptianhieroglyphs.net/">Alec's website</a>
+    <p> This is a website created by an egyptologist researcher. The content is similar to ours but this covers coptic as well. If you want to get into deciphering texts this is good place to start.</p>
+  </div>
+  <hr>
+  <div>
+    <!-- Resource 2-->
+    
+  </div>
+  <hr>
+  <div>
+    <!-- Resource 3-->    
+    <h1>While there are few free resouces available for learning Egpytian we believe that it's </h1>
+  </div>
+  <hr>
+  <div>
+    <!-- Resource 4-->    
+    <h1>While there are few free resouces available for learning Egpytian we believe that it's </h1>
+  </div>
+  <hr>
+  <div>
+    <!-- Resource 5-->    
+    <h1>While there are few free resouces available for learning Egpytian we believe that it's </h1>
+  </div>
+  <hr>
+  <span>&copy2023;</span>
+</body>
+</html>
