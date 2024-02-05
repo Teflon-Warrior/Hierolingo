9ddc2e962f009e554be823f815c1f534045b289d
Luke Harris <johaharr@iu.edu>
2024-02-05 00:08:51 -0500

    bootstrap isn't working.

--- lesson2.php
+++ lesson2.php
@@ -1,9 +1,9 @@
 <html>
 
 <head>
-  <!-- bootstrap css-->
+  <!-- bootstrap css
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
+-->
   <link href="./css/lessoncss.css" rel="stylesheet" type="text/css">
   <link rel="stylesheet" type="text/css" href="css/general.css"/>
   <script type="text/javascript" src="./js/lessonjs.js"></script>
