f14068ecd8c644de7633f4a2093a52fb1f517e40
Luke Harris <johaharr@iu.edu>
2024-02-04 23:48:49 -0500

    forgot to include resource.php in previous commit.

--- resource.php
+++ resource.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/general.css"/>
 </head>
 <body>
     <h1> some additional resources to access to learn the egpyitan Language</h1>
