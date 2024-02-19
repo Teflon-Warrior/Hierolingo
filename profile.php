4a2203c149cfef74c9e56db8cb7b2afa5e79afd6
Aidan Smith <aidhsmit@iu.edu>
2024-02-19 00:36:07 -0500

    Fixing profile sidenav

--- profile.php
+++ profile.php
@@ -73,7 +73,12 @@
                     <li><a href="logout.php">Log Out</a></li>
                 </ul>
             </nav>
-
+            <header>
+                <div class="openbtn">
+                    <span class="material-symbols-outlined menu-button">menu</span>
+                    <span class="menu-text">menu</span>
+                </div>
+                <div class="all-over-bkg"></div>
             <h1>Profile</h1>
 
         </header>
