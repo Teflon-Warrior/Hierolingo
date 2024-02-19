3e32ec82c6da85061eb4e48da6b69e9cc23b7582
Aidan Smith <aidhsmit@iu.edu>
2024-02-19 01:17:52 -0500

    Adjusting css for lesson page

--- profile.php
+++ profile.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+</head>
 
     <?php
     //config.php includes information where we open the database
