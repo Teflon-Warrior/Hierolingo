0315285231e28b08e51aa8e66acd0ba56274f892
Aidan Smith <aidhsmit@iu.edu>
2024-02-29 17:39:52 -0500

    Reversed to og form, didnt work and dont have time to fix, will do later

--- studysets.php
+++ studysets.php
@@ -13,42 +13,41 @@
 		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 	<link rel="stylesheet" href="css/tabbingStyling.css" />
 
-	<?php
-	require 'config.php';
-	session_start();
-	$con = $db_connection;
-	if (isset($_SESSION['login_id']) == null) {
-		header('Location: https://cgi.luddy.indiana.edu/~team11/team-11/login.php');
-	}
+<?php
+        require 'config.php';
+        session_start();
+        $con = $db_connection;
+if (isset($_SESSION['login_id']) == null) {
+        header( 'Location: https://cgi.luddy.indiana.edu/~team11/team-11/login.php');
+}
 
-	$google_id = $_SESSION['login_id'];
-	$con = mysqli_connect("db.luddy.indiana.edu", "i494f23_team11", "my+sql=i494f23_team11", "i494f23_team11");
+$google_id = $_SESSION['login_id'];
+$con = mysqli_connect("db.luddy.indiana.edu" ,"i494f23_team11","my+sql=i494f23_team11","i494f23_team11");
 
-	$query = "Select userlevel from User where google_id = $google_id";
-	$result = mysqli_query($con, $query);
-	$result = mysqli_fetch_array($result);
+$query = "Select userlevel from User where google_id = $google_id";
+$result = mysqli_query($con, $query);
+$result = mysqli_fetch_array($result);
 
-	$les = $result['userlevel'];
+$les = $result['userlevel'];
 
-	?>
+?>
 
 </head>
-
 <body>
-
+	
 	<!-- NAVIGATION -->
 	<nav id="mySidenav" class="sidenav">
 		<ul>
 			<li><a class="closebtn">&times;</a></li>
 			<li><a href="profile.php">Profile</a></li>
-			<li><a href="lesson.php<?php echo "?les=$les"; ?>">Lessons</a></li>
+			<li><a href="lesson.php<?php echo"?les=$les";?>">Lessons</a></li>			
 			<li><a href="dictionary.php">Dictionary</a></li>
 			<li><a href="studysets.php">Study Sets</a></li>
 			<li><a href="leaderboard.php">Leaderboard</a></li>
-			<li><a href="logout.php">Log Out</a></li>
+			<li><a href="logout.php">Log Out</a></li>			
 		</ul>
 	</nav>
-
+	
 	<!-- MAIN CONTENT -->
 	<header>
 		<div class="openbtn">
@@ -58,17 +57,17 @@
 		<div class="all-over-bkg"></div>
 		<h1>Study Sets</h1>
 	</header>
-
+	
 	<!-- PHP connection & Queries -->
 	<?php
-
+	
 	//get userID
-	$userIDQuery = "SELECT id FROM User where google_id = " . $_SESSION['login_id'] . ";";
-	$userIDResult = mysqli_fetch_array(mysqli_query($db_connection, $userIDQuery), MYSQLI_NUM);
-	$userID = $userIDResult[0];
+ 	$userIDQuery = "SELECT id FROM User where google_id = ".$_SESSION['login_id'].";";
+        $userIDResult = mysqli_fetch_array(mysqli_query($db_connection, $userIDQuery), MYSQLI_NUM);
+        $userID = $userIDResult[0];
 
-	//This query will pull the relevant JSON objects for getting the title and wordIDs for each study set.
-	$setTabsQuery = "SELECT listname FROM vocablist WHERE ID = " . $userID . ";";
+        //This query will pull the relevant JSON objects for getting the title and wordIDs for each study set.
+        $setTabsQuery = "SELECT listname FROM vocablist WHERE ID = ".$userID.";";	
 
 	//Result section
 	$setTabsResult = mysqli_query($con, $setTabsQuery);
@@ -76,91 +75,87 @@
 	$setWordsResult = "";
 	//Number of tabs for forloop
 	$numTabs = count($setTabsResult);
-
-	function displayQueryResults($wordsIn, $con)
-	{
-		if (mysqli_connect_errno()) {
-			die("Failed to connect to MySQL: " . mysqli_connect_error());
-		} else {
-		}
+	
+	function displayQueryResults($wordsIn, $con){	
+		if (mysqli_connect_errno())
+			{ die("Failed to connect to MySQL: " . mysqli_connect_error()); }
+		else
+			{}
 		if (count($wordsIn) > 0) {
-			echo "<table border = '1'>
+				echo "<table border = '1'>
 				<tr>
 					<th>Heiroglyph</th>
 					<th>Definition </th>
 					<th>Part of Speech</th>
 				</tr>";
-			for ($i = 0; $i < count($wordsIn); $i++) {
-				$printQuery = "SELECT * FROM dictionary WHERE id = " . $wordsIn[$i] . ";";
-				$results = mysqli_fetch_array(mysqli_query($con, $printQuery), MYSQLI_NUM);
-				echo "
+				for ($i = 0; $i < count($wordsIn); $i++ ){
+					$printQuery = "SELECT * FROM dictionary WHERE id = ".$wordsIn[$i].";";
+					$results = mysqli_fetch_array(mysqli_query($con, $printQuery), MYSQLI_NUM);
+					echo "
 					<tr>
-						<td><img src = " . $results[3] . " width='200' height='200' /> </td> 
-						<td>" . $results[2] . "</td> 					
-						<td>" . $results[1] . "</td>
+						<td><img src = ".$results[3]." width='200' height='200' /> </td> 
+						<td>".$results[2]."</td> 					
+						<td>".$results[1]."</td>
 					</tr>";
+				}		
+			
+				echo "</table>";
 			}
-
-			echo "</table>";
-		}
 	}
 	?>
-
+	
 	<script src="js/nav.js"></script>
-
-
+	
+	
 	<!-- This goes through and displays tabs based on how many are asssociated with the user -->
 	<script type="text/javascript" src="js/dictionaryDisplayLesson.js"></script>
 	<div class="tabs">
-		<?php
+	<?php
 		//Set up tab headers
-		while ($setTabs = mysqli_fetch_array($setTabsResult, MYSQLI_NUM)) {
-			echo "<button class='lessontab' onclick='displayLesson(event, \"" . $setTabs[0] . "\");'>" . $setTabs[0] . "</button>\n";
+		while  ($setTabs =  mysqli_fetch_array($setTabsResult, MYSQLI_NUM)){
+			echo "<button class='lessontab' onclick='displayLesson(event, \"".$setTabs[0]."\");'>".$setTabs[0]."</button>\n";
 		}
 		//Add header
 		echo "<button class='lessontab' onclick='displayLesson(event, \"Add\");'>Add New Set</button>";
-		?>
+	?>
 	</div>
-
+	
 	<!-- This will set the tabs to display each word within a user list. -->
-	<?php
-	mysqli_data_seek($setTabsResult, 0);
-	while ($setTabs = mysqli_fetch_array($setTabsResult, MYSQLI_NUM)) {
-		echo "<div id = '" . $setTabs[0] . "' class = 'lessonContent'>";
-
-		$setWordsQuery = "SELECT filepath FROM vocablist WHERE listname = '" . $setTabs[0] . "' and ID = " . $userID . ";";
+<?php 
+	mysqli_data_seek($setTabsResult, 0);	
+	while  ($setTabs =  mysqli_fetch_array($setTabsResult, MYSQLI_NUM))	{
+		echo "<div id = '".$setTabs[0]."' class = 'lessonContent'>";
+	
+		$setWordsQuery = "SELECT filepath FROM vocablist WHERE listname = '".$setTabs[0]."' and ID = ".$userID.";";
 		$setWordsResult = mysqli_fetch_array(mysqli_query($db_connection, $setWordsQuery), MYSQLI_NUM);
 		$words = file_get_contents($setWordsResult[0]);
 		$words = json_decode($words);
 		displayQueryResults($words, $db_connection);
 		echo "</div>";
 	}
-
-	echo "<form action='addStudySet.php' method='post'>
-			<div class='form-group'>
-  				<label for='exampleInputEmail1'>What would you like to name your study set?</label>
-  				<input type='text' name='studySetName' class='form-control' placeholder='Type a name...'><br>
-			</div>
-			<button type='submit' class='btn btn-primary'>Submit</button>
-		</form>";
-
+	
+	echo "<div id = 'Add' class = 'lessonContent'>";
+	echo "<h3>Add a new Study Set</h3>";
+	echo "<form action = 'addStudySet.php' method = 'post'>
+		What would you like to name your study set? <input type = 'text' name = 'studySetName'><br>
+		<input type = 'submit'>	
+	     </form>";
+	echo "</div>";
 	?>
 
-
+	
 </body>
-
 </html>
 
 <!---
 <?php
 
-echo "<div id = 'Add' class = 'lessonContent'>";
-echo "<h3>Add a new Study Set</h3>";
-echo "<form action = 'addStudySet.php' method = 'post'>
-	What would you like to name your study set? <input type = 'text' name = 'studySetName'><br>
-	<input type = 'submit'>	
-	 </form>";
-echo "</div>";
-
+echo "<form action='addStudySet.php' method='post'>
+<div class='form-group'>
+  <label for='exampleInputEmail1'>What would you like to name your study set?</label>
+  <input type='text' name='studySetName' class='form-control' placeholder='Type a name...'><br>
+</div>
+<button type='submit' class='btn btn-primary'>Submit</button>
+</form>";
 
 ?> -->
