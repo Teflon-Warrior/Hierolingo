42e9ad070f647bcb7f093cafc7c8ebdcd99d8d19
Luke Harris <johaharr@iu.edu>
2024-03-03 13:53:25 -0500

    quiz2 basic outline created

---
+++ quiz2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- bootstrap css-->
+  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">-->
+
+  <link href="./css/lessoncss.css" rel="stylesheet" type="text/css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/styles.css">
+  <link rel="stylesheet" href="css/lesson.css">
+  <link rel="stylesheet" type="text/css" href="css/general.css" />
+  <script type="text/javascript" src="./js/lessonjs.js"></script>
+
+
+</head>
+<body>
+<nav id="mySidenav" class="sidenav">
+    <ul>
+      <li><a class="closebtn">&times;</a></li>
+      <li><a href="profile.php">Profile</a></li>
+      <li><a href="lesson.php">Lessons</a></li>
+      <li><a href="dictionary.php">Dictionary</a></li>
+      <li><a href="studysets.php">Study Sets</a></li>
+      <li><a href="leaderboard.php">Leaderboard</a></li>
+      <li><a href="logout.php">Log Out</a></li>
+    </ul>
+  </nav>
+  <header>
+		<div class="openbtn">
+			<span class="material-symbols-outlined menu-button">menu</span>
+			<span class="menu-text">menu</span>
+		</div>
+		<div class="all-over-bkg"></div>
+		<h1>Lesson</h1>
+	</header>
+  <?php
+  session_start();
+  ?>
+  <nav class="navbar navbar-expand-sm bg-dark">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" style="color:white;" href="profile.php">Profile</a>
+      </li>
+    
+</body>
+</html>
