14b5772fe0a36bb74b0ac352ba3f6efac6e756e5
maxarno <maxarno@iu.edu>
2024-03-04 11:57:39 -0500

    fixing pathing in js file

--- js/lessonjs.js
+++ js/lessonjs.js
@@ -40,6 +40,11 @@ function lessonnavClicked(les, i) {
 //Next and previous lesson buttons
 function prevlessonClicked(les) {
         //ensures that the user can't keep going backwards
+	var les = les-1;
+	ocation.replace("https://cgi.luddy.indiana.edu/~team11/team-11/lesson.php?les=" + les);
+	
+
+/* There was already an if statement in the php file where the prev button doesnt show if its lesson 1
         if (!(les <= 1)) {
         var les = les-1;
         location.replace("https://cgi.luddy.indiana.edu/~team11/team-11/lesson.php?les=" + les);
@@ -48,11 +53,12 @@ function prevlessonClicked(les) {
                 les = 1
                 location.replace("https://cgi.luddy.indiana.edu/~team11/team-11/lesson.php?les=" + les);
         }
+*/
 }
 
 function nextlessonClicked(les) {
         var les = les+1;
-        location.replace("https://cgi.luddy.indiana.edu/~team11/team-11/quiz" + les + ".php");
+        location.replace("https://cgi.luddy.indiana.edu/~team11/team-11/lesson.php?les=" + les);
 }
 
 //Take quiz button
