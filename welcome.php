3602ab6930d96ca91b03e18ae30d81fe5cbe6cbe
Aidan Smith <aidhsmit@iu.edu>
2024-03-05 00:15:53 -0500

    Forgot to link bootstrap

--- welcome.php
+++ welcome.php
@@ -5,20 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome!</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+                integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+                crossorigin="anonymous">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/welcome.css">
 </head>
 
 <body>
-    <div class="jumbotron">
-        <h1 class="display-4">DataGlyph</h1>
-        <p class="lead">Heiroglyphics, easier.</p>
-        <hr class="my-4">
-        <p>Learn ancient egyptian heiroglyphics like never before.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="#" role="button">Log In</a>
-        </p>
+    <div class='container'>
+        <div class="jumbotron">
+            <h1 class="display-4">DataGlyph</h1>
+            <p class="lead">Heiroglyphics, easier.</p>
+            <hr class="my-4">
+            <p>Learn ancient egyptian heiroglyphics like never before.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" href="#" role="button">Log In</a>
+            </p>
+        </div>
     </div>
 
     <div class="buttons">
