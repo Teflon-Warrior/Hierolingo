b99421f1fcde6565cac1920374465bfe79351aaa
Capstone_team11 <team11@silo.luddy.indiana.edu>
2024-03-04 23:25:46 -0500

    got rid of weird styling

--- lesson.php
+++ lesson.php
@@ -17,27 +17,10 @@ $color = $result['color'];
 ?>
 
         <!-- bootstrap css-->
-        <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">-->
+       <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">-->
 
         <script type="text/javascript" src="./js/lessonjs.js"></script>
-        <meta charset="UTF-8">
-
-    
-          
-            
-    
-
-          
-          Expand Down
-          
-            
-    
-
-          
-          Expand Up
-    
-    @@ -65,6 +64,17 @@ $color = $result['color'];
-  
+        <meta charset="UTF-8">  
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Lessons</title>
