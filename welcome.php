e48b6d1f0641327c24d512a61c153612678d318d
Aidan Smith <aidhsmit@iu.edu>
2024-03-05 00:44:06 -0500

    changing padding and others

--- welcome.php
+++ welcome.php
@@ -22,7 +22,7 @@
         </div>
     </div>
 
-    <div class="container">
+    <div class="container-columns">
         <!-- Example row of columns -->
         <div class="row">
             <div class="col-md-4">
@@ -45,9 +45,6 @@
 
     </main>
 
-    <footer class="container">
-        <p>&copy; Company 2017-2019</p>
-    </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
@@ -55,12 +52,6 @@
     <script src="/docs/4.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o"
         crossorigin="anonymous"></script>
-</body>
-
-</html>
-
-
-
 </body>
 
 </html>
