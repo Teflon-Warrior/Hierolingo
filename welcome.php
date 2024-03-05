82a1f0b1c9a33fdfea81064804dd5d1c41b08158
Aidan Smith <aidhsmit@iu.edu>
2024-03-05 00:45:48 -0500

    changing div tag

--- welcome.php
+++ welcome.php
@@ -13,35 +13,45 @@
 </head>
 
 <!-- Homepage layout from Bootstrap (https://getbootstrap.com/docs/4.3/examples/jumbotron/) -->
+
 <body>
     <div class="jumbotron">
         <div class="container">
             <h1 class="display-3">DataGlyph</h1>
             <p>Heiroglyphics, now easier.</p>
-            <p><a class="btn btn-primary" href="https://cgi.luddy.indiana.edu/~team11/team-11/login.php" role="button">Log In &raquo;</a></p>
+            <p><a class="btn btn-primary" href="https://cgi.luddy.indiana.edu/~team11/team-11/login.php"
+                    role="button">Log In &raquo;</a></p>
         </div>
     </div>
 
-    <div class="container-columns">
-        <!-- Example row of columns -->
-        <div class="row">
-            <div class="col-md-4">
-                <h2>Advanced Learning</h2>
-                <p>DataGlyph uses advanced learning modules to enhance the learning experience of the user. Registered users can utilize a dictionary of words, customizable study sets, and flashcard-based lessons to assist them in learning the ancient writing. </p>
+    <div class='columns-div'>
+        <div class="container">
+            <!-- Example row of columns -->
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>Advanced Learning</h2>
+                    <p>DataGlyph uses advanced learning modules to enhance the learning experience of the user.
+                        Registered users can utilize a dictionary of words, customizable study sets, and flashcard-based
+                        lessons to assist them in learning the ancient writing. </p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Quizzes</h2>
+                    <p>Users of DataGlyph can partake in quizzes that if passed, advances them to a new level. With new
+                        levels come new words to learn!</p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Notifications</h2>
+                    <p>DataGlyph utilizes the Twilio API to notify users (permission granted) when to practice their
+                        heiroglyphics. This way, you can stay on track to learning egyptian heiroglyphics without
+                        forgetting!</p>
+                </div>
             </div>
-            <div class="col-md-4">
-                <h2>Quizzes</h2>
-                <p>Users of DataGlyph can partake in quizzes that if passed, advances them to a new level. With new levels come new words to learn!</p>
-            </div>
-            <div class="col-md-4">
-                <h2>Notifications</h2>
-                <p>DataGlyph utilizes the Twilio API to notify users (permission granted) when to practice their heiroglyphics. This way, you can stay on track to learning egyptian heiroglyphics without forgetting!</p>
-            </div>
-        </div>
 
-        <hr>
+            <hr>
 
-    </div> <!-- /container -->
+        </div> <!-- /container -->
+
+    </div>
 
     </main>
 
