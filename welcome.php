2165727427931ef62b3487e61a9d7e97f1d24ee4
Aidan Smith <aidhsmit@iu.edu>
2024-03-05 00:22:34 -0500

    fixing layout

--- welcome.php
+++ welcome.php
@@ -18,7 +18,7 @@
         <div class="container">
             <h1 class="display-3">DataGlyph</h1>
             <p>Heiroglyphics, now easier.</p>
-            <p><a class="btn btn-primary btn-lg" href="https://cgi.luddy.indiana.edu/~team11/team-11/login.php" role="button">Log In &raquo;</a></p>
+            <p><a class="btn btn-primary" href="https://cgi.luddy.indiana.edu/~team11/team-11/login.php" role="button">Log In &raquo;</a></p>
         </div>
     </div>
 
@@ -30,21 +30,18 @@
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                     mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
                     mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Heading</h2>
                 <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor
                     mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna
                     mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
             <div class="col-md-4">
                 <h2>Heading</h2>
                 <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula
                     porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh,
                     ut fermentum massa justo sit amet risus.</p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
             </div>
         </div>
 
