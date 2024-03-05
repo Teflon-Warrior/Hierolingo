36202081dbfe98054873ec800843949e137c13e6
Aidan Smith <aidhsmit@iu.edu>
2024-03-04 23:55:59 -0500

    Fixing edit css

--- edit.php
+++ edit.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Dictionary</title>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/normalize.css">
-	<link rel="stylesheet" href="css/styles.css">
-	<link rel="stylesheet" href="css/edit.css">
-	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-	<link rel="stylesheet" href="css/tabbingStyling.css" />
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/edit.css">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="css/tabbingStyling.css" />
 
     <?php
     require "config.php";
     session_start();
-    if(isset($_SESSION['login_id'])) {
+    if (isset($_SESSION['login_id'])) {
         // Assuming you have already established a database connection
         $query = "SELECT username, userhandle, profile_image FROM User WHERE google_id = '{$_SESSION['login_id']}'";
-    
+
         // Execute your query here...
     } else {
         // Handle the case where $_SESSION['login_id'] is not set
@@ -30,31 +32,65 @@
 
     if (mysqli_num_rows($result) > 0) {
         if (mysqli_num_rows($result) > 0) {
-          while ($row = mysqli_fetch_assoc($result)) {
+            while ($row = mysqli_fetch_assoc($result)) {
 
-            $username = $row['username'];
-            $userhandle = $row['userhandle'];
-            $image = $row['profile_image'];
-            //echo $image;
-
-        }
-    } else {echo "no results";}
-}
-    ?>
+                $username = $row['username'];
+                $userhandle = $row['userhandle'];
+                $image = $row['profile_image'];
+                //echo $image;
     
+            }
+        } else {
+            echo "no results";
+        }
+    }
+    ?>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit</title>
 </head>
-<body>
-    <form action="update.php" method="POST">
-      Username: <input type="text" name="name" value="<?php echo $username?>" class=""><br>
-      Userhandle: <input type="text" name="handle" value="<?php if ($userhandle != NULL) {echo $userhandle; } ?>" class=''><br>
-     profile image: <input type="text" name="image" value="<?php echo $image?>" ><br>
-     <br>
-    <input type="submit" class="">
-    </form>
 
-    
+<body>
+    <header>
+        <div class="openbtn">
+            <span class="material-symbols-outlined menu-button">menu</span>
+            <span class="menu-text">menu</span>
+        </div>
+        <div class="all-over-bkg"></div>
+        <h1>Edit</h1>
+    </header>
+
+    <div class="edit-form">
+        <form action="update.php" method="POST">
+            <div class="form-group row">
+                <label for="inputUsername" class="col-sm-2 col-form-label">Username:</label>
+                <div class="col-sm-10">
+                    <input type="text" name="name" value="<?php echo $username ?>" class=""><br>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputHandle" class="col-sm-2 col-form-label">Handle:</label>
+                <div class="col-sm-10">
+                    <input type="text" name="handle" value="<?php if ($userhandle != NULL) {
+                        echo $userhandle;
+                    } ?>" class=''><br>
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="inputImage" class="col-sm-2 col-form-label">Profile Image:</label>
+                <div class="col-sm-10">
+                    <input type="text" name="image" value="<?php echo $image ?>"><br>
+                </div>
+            </div>
+            <br>
+            <button class="btn btn-primary" type="submit" id="edit" value="Save Changes">Save Changes</button>
+    </div>
+    </form>
+    </div>
+
+
+
 </body>
+
 </html>
