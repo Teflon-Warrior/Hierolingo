1aa008962addebd1b96c4d5419126edbddbeace7
maxarno <maxarno@iu.edu>
2024-03-26 11:56:03 -0400

    Making studysets mobile responsive

--- studysets.php
+++ studysets.php
@@ -57,7 +57,7 @@ $les = $result['userlevel'];
 			<span class="menu-text">menu</span>
 		</div>
 		<div class="all-over-bkg"></div>
-		<h1>Study Sets</h1>
+		<div style="padding-top: 40px; position: absolute; left: 50%; transform: translate(-50%,0); font-size: 2em;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: white;font-weight: bold;"><div class="study"> Study Sets </div></div>
 	</header>
 	
 	<!-- PHP connection & Queries -->
