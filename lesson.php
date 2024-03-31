efdd849e84f6c11e11c99f867a78764f5e26ddf3
Luke Harris <johaharr@iu.edu>
2024-03-31 14:26:37 -0400

    changes

--- lesson.php
+++ lesson.php
@@ -17,27 +17,27 @@
         $color = $result['color'];
         ?>
 
-        <!-- bootstrap css-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-                crossorigin="anonymous">
 
-        <script type="text/javascript" src="./js/lessonjs.js"></script>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Lessons</title>
+        <!-- bootstrap css-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="css/general.css" />
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="css/tabbingStyling.css" />
         <link rel="stylesheet" href="css/lesson.css">
         <link rel="stylesheet"
-                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&display=swap"
-                rel="stylesheet">
+        rel="stylesheet">
+        <script type="text/javascript" src="./js/lessonjs.js"></script>
 </head>
 
 <body>
@@ -172,13 +172,13 @@
         }
         echo "</div>";
 
-        echo "<div class='flash' onclick='termClick($id);' id='term_$id'>";
+        echo "<div class='flash1' onclick='termClick($id);' id='term_$id'>";
         //echo $filepath;
         echo "<img src='$filepath'>";
         echo "</div>";
 
 
-        echo "<div class='flash' hidden onclick='definitionClick($id);' id='definition_$id'>";
+        echo "<div class='flash1' hidden onclick='definitionClick($id);' id='definition_$id'>";
         echo "<p class='flashText1'>'$def'</p>";
         echo "</div>";
 
