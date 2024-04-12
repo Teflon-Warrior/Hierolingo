bacf8abf29e17680512c9c7475b5610a47005ce9
Hochgesang, Jeff Thomas <jefhochg@iu.edu>
2024-04-12 16:33:16 -0400

    Initial lessonsHome commit
    This has basic html and css. I may need to go back and adjust this depending on how the visual redesign is done.

---
+++ lessonsHome.php
@@ -0,0 +1,69 @@
+<html>
+	<head>
+		<title>Lessons Home</title>
+		 <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Lessons Home</title>
+	
+		<script type="text/javascript" src="./js/profile.js"></script>
+
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/profile.css">
+		<!--<link rel="stylesheet" href="css/profile.css">-->
+		<link rel="stylesheet" href="css/tabbingStyling.css" />
+
+		<link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	</head>
+	<body>
+	 <div class="profile-header">
+
+        <header>
+            <nav id="mySidenav" class="sidenav">
+                <ul>
+                    <li><a class="closebtn">&times;</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="lesson.php<?php echo"?les=$level";?>">Lessons</a></li>
+                    <li><a href="dictionary.php">Review</a></li>
+                    <li><a href="studysets.php">Study Sets</a></li>
+		    <li><a href="settings.php">Settings</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>
+            </nav>
+            <header>
+                <div class="openbtn">
+                    <span class="material-symbols-outlined menu-button">menu</span>
+                    <span class="menu-text">menu</span>
+                </div>
+                <div class="all-over-bkg"></div>
+                <div style="padding-top: 40px; position: absolute; left: 50%; transform: translate(-50%,0); font-size: 2em;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: white;font-weight: bold;">Profile </div>
+
+            </header>
+    </div>	
+		
+		<h1>Lesson Home</h1>
+		<p>Here you'll find our lesson content, flashcards, and quizzes. Click on one to get started!</p>
+		<p>The learn button will take you to our grammar resources, while flashcards will take you to a place 
+		where you can learn vocabulary. When you feel comfortable with both sections in a lesson, take the quiz 
+		to test yourself! </p>
+		
+		<h3>Lesson 1 </h3>
+		<a href = "l1grammar.php"><p>Learn</p></a> <p> | </p>
+		<a href = "lesson.php?les=1"><p>Flashcards</p></a> <p> | </p>
+		<a href = "quiz.php"><p>Quiz</p></a>
+		<h3>Lesson 2 </h3>
+		<a href = "l2grammar.php"><p>Learn</p></a> <p> | </p>
+		<a href = "lesson.php?les=2"><p>Flashcards</p></a> <p> | </p>
+		<a href = "quiz.php"><p>Quiz</p></a>
+		<h3>Lesson 3 </h3>
+		<a href = "l3grammar.php"><p>Learn</p></a> <p> | </p>
+		<a href = "lesson.php?les=3"><p>Flashcards</p></a> <p> | </p>
+		<a href = "quiz.php"><p>Quiz</p></a>
+		<h3>Lesson 4 </h3>
+		<a href = "l4grammar.php"><p>Learn</p></a> <p> | </p>
+		<a href = "lesson.php?les=4"><p>Flashcards</p></a> <p> | </p>
+		<a href = "quiz.php"><p>Quiz</p></a>
+	</body>
+</html>
