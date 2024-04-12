23377e6759e06b21229d178de3dff845410ab0a8
maxarno <maxarno@iu.edu>
2024-04-12 19:09:59 -0400

    fixing flashcard in lesson

--- lesson.php
+++ lesson.php
@@ -23,10 +23,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Lessons</title>
         <!-- bootstrap css-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+       <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
         crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="css/general.css" />
+        -->
+	<link rel="stylesheet" type="text/css" href="css/general.css" />
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/styles.css">
         <link rel="stylesheet" href="css/tabbingStyling.css" />
