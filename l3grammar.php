797b16bd854ecb6ec21107328a3e7d61667f83cd
jefhochg <jefhochg@iu.edu>
2024-04-12 20:36:53 -0400

    l3grammar added

---
+++ l3grammar.php
@@ -0,0 +1,96 @@
+<html>
+	<head>
+		<title>Lesson 3</title>
+		 <meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Lesson 3</title>
+	
+		<script type="text/javascript" src="./js/profile.js"></script>
+
+		<link rel="stylesheet" href="css/normalize.css">
+		<link rel="stylesheet" href="css/styles.css">
+		<link rel="stylesheet" href="css/profile.css">
+		<!--<link rel="stylesheet" href="css/profile.css">-->
+		<link rel="stylesheet" href="css/tabbingStyling.css" />
+
+		<link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	</head>
+	<body>
+	 <div class="profile-header">
+
+        <header>
+            <nav id="mySidenav" class="sidenav">
+                <ul>
+                    <li><a class="closebtn">&times;</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="lessonsHome.php">Lessons</a></li>
+                    <li><a href="dictionary.php">Review</a></li>
+                    <li><a href="studysets.php">Study Sets</a></li>
+					<li><a href="settings.php">Settings</a></li>
+                    <li><a href="logout.php">Log Out</a></li>
+                </ul>
+            </nav>
+            <header>
+                <div class="openbtn">
+                    <span class="material-symbols-outlined menu-button">menu</span>
+                    <span class="menu-text">menu</span>
+                </div>
+                <div class="all-over-bkg"></div>
+                <div style="padding-top: 40px; position: absolute; left: 50%; transform: translate(-50%,0); font-size: 2em;font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif; color: white;font-weight: bold;">Profile </div>
+
+            </header>
+    </div>	
+	
+	<h1> Lesson 3 </h1>
+	<p> I hope you love grammar, because today there is going to be a lot of grammar talk. 
+	We’ll talk declension, adjectives, suffixes, and verb tenses. Don’t worry quite yet if these words are nonsense to you, 
+	we’ll go through and define them. 
+	</p>
+	
+	<h3> Declension </h3>
+	<p>Our first topic is declension. Declension might be completely new to you as an English speaker, 
+	through many other languages show heavy declension. Declension is the process of changing a word to show grammatical distinctions, 
+	such as case, tense, number, and many other things. In English, only our pronouns really decline. 
+	You would say “I saw him” rather than “I saw he”, or “It is mine” to indicate you own something, rather than “It is I”. 
+	Using he insteam of him or him instead of he is due to declension.
+	Egyptian, as talked about last lesson, has declensions for gender and number for nouns. 
+	To show how words decline, we’ll be putting them in a cool table for you. </p>
+	<!-- INSERT TABLE FROM EME_Chap3Summary SLIDE 6 --> 
+	<p>I’m sure you may recognize some of these symbols from the previous lesson and some of the words you have learned. 
+	These are obligatory to add. However, there does exist a case with logograms(Remember those? 
+	Head back to Lesson 2 if you need a refresher), where if you need to use the dual to indicate you have 2 of something, 
+	you can just write the logogram twice.  
+	</p>
+	
+	<h3> Adjectives </h3>
+	<p> Adjectives also have declension in Egyptian. Luckily for you, they follow the exact same pattern as nouns. 
+	When using adjectives they follow the noun they modify. Think of it as “a ball blue” rather than “a blue ball”
+	You can also use adjectives as nouns. This is like saying “A good one” from the adjective “good”. To do this, take your adjective, and attach the feminine singular ending to it. This turns the adjective into a noun. 
+	</p>
+	
+	<h3>Possession</h3>
+	<p>Showing possession of something is incredibly important in expressing ideas. 
+	“My friend’s dog” or “The boat’s interior” or even “The President of the United States” all require some sort of possession, also known the genitive, 
+	to work. Egyptian has three ways of making this work. The first is known as the bound construction. You can place one noun directly after another to show that the first possesses the second. 
+	You can think of the English word “Homeowner” to see a similar example, though in Egyptian you’d want to say “ownerhome”. You can think of the second noun in Egyptian as a modifier for the first noun. 
+	The second form is called the Indirect Genitive, which makes use of a preposition, which translates as of. This preposition sort of acts like an adjective, in that it declines the same ways. Here’s a table showing the forms:
+    <!--INSERT TABLE FROM EME_Chap3Summary SLIDE 15 -->
+	
+	<p>You should watch out that the masculine singular has the same appearance as the preposition “as/for”. Normally the context will determine which preposition is being used. 
+	The third way is through possessive pronouns, which are like suffixes in Egyptian. Nothing can come between these suffixes and the noun, they must be together! Once again they decline. Note the common column.
+	For these, you only have to remember one, and are used regardless of the gender of what it is attached to. The closest way of translating this would be like “My house” or “His boat”, instead of two words, you add on to the base noun.
+	</p>
+	<!-- INSERT TABLE FROM SLIDE 18 HERE -->
+	
+	<p>	
+	There a lot of different ways you’ll see these suffixes used, and while it would be possible to go over each one, it would be much more beneficial to keep your eyes open and learn the contexts for yourself.
+	</p>
+	
+	<br>
+	<p> As always, theres new vocabulary below. After looking at that, move onto the quiz.</p>
+	<button onclick="lesson.php?les=3">Flashcards</button>
+	<button onclick="quiz3.php">Quiz</button>
+	</body>
+</html>
