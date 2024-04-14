9933db57bd04ed5f2ecc18dfd5f43b472feb2c1f
jefhochg <jefhochg@iu.edu>
2024-04-14 17:03:17 -0400

    fixing navbar

--- l1grammar.php
+++ l1grammar.php
@@ -6,7 +6,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Lesson 1</title>
 	
-		<script type="text/javascript" src="./js/profile.js"></script>
+		<script src="js/nav.js"></script>
 
 		<link rel="stylesheet" href="css/normalize.css">
 		<link rel="stylesheet" href="css/styles.css">
