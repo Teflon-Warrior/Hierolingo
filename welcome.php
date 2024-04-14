624e9e1306941773d89cd91209ea13f0bf61ac26
Aidan Smith <aidhsmit@iu.edu>
2024-04-14 11:04:27 -0400

    adding exo font to welcome

--- welcome.php
+++ welcome.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="stylesheet" href="css/welcome.css">
+    <link rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&display=swap"
+		rel="stylesheet">
 </head>
 
 <!-- Homepage layout from Bootstrap (https://getbootstrap.com/docs/4.3/examples/jumbotron/) -->
